952f4dd1f9b1adade49b9aa00b028dd016936689
Joni Korpelin <jonikorpelin@gmail.com>
2015-03-12 12:52:42 +0200

    täs tätä

--- client/scripts/components/scoreboard.jsx
+++ client/scripts/components/scoreboard.jsx
@@ -4,15 +4,19 @@ var React = require('react');
 var DefaultLayout = require('./layouts/default.jsx');
 var BS = require('react-bootstrap');
 
+
+var data = [
+  {First: 'Nadir', last: 'Derdour', points: '-9000'},
+  {First: 'Aapo', last: 'Salo', points: '20'},
+]
+
 var ScoreboardComponent = React.createClass({
   render: function() {
     return (
       /* jshint ignore:start */
       <DefaultLayout>
         <BS.Table className="shittable">
-          <Person first="Apina" last="paske" points="120" />
-          <Person first="Nadir" last="Derdour" points="-9000" />
-          <BS.Button bsStyle="primary">Primary</BS.Button>
+          <PersonList data={this.props.data} />
         </BS.Table>
       </DefaultLayout>
       /* jshint ignore:end */
@@ -35,9 +39,23 @@ var Person = React.createClass({
           <h2>{this.props.points}</h2>
         </td>
       </tr>
+      /* jshint ignore:end */
       )
   }
+});
 
+var PersonList = React.createClass({
+  render: function() {
+        var people = this.props.data.map(function(person){
+          return <Person first={person.first} last={person.last} person={person.points} />
+        });
+        return (
+          <div>
+          {people}
+          </div>
+          )
+  }
 })
+
 
 module.exports = ScoreboardComponent;
